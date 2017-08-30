6a3f584357c5f522ed729c0fe992e236091f6c58
Matthew Shield <matthewshield@hotmail.co.uk>
2017-08-30 18:24:46 +0100

    finished addtional tests

--- array_tasks.js
+++ array_tasks.js
@@ -1,24 +1,33 @@
 var arrayTasks = {
 
-	// concat: function (arr1, arr2) {
+	concat: function (arr1, arr2) {
+		return arr3 = arr1.concat(arr2);
+	},
 
-	// },
 
-	// insertAt: function (arr, itemToAdd, index) {
-		
-	// },
+	insertAt: function (arr, itemToAdd, index) {
+		arr.splice(index, 0, itemToAdd);
+		return arr;
+	},
 
-	// square: function (arr) {
-		
-	// },
+	square: function (arr) {
+		return arr.map( function(element) {
+			return element ** 2;
+		})
+	},
 
-	// sum: function (arr) {
-		
-	// },
+	sum: function (arr) {
+		var total = 0;
+		arr.forEach( function(element) {
+		  total += element;
+		} )
+		return total;
+	},
 
-	// findDuplicates: function (arr) {
-		
-	// },
+//should find duplicate values in an array, returning a new array of the duplicates
+	findDuplicates: function (arr) {
+	
+	},
 
 	// removeAndClone: function (arr, valueToRemove) {
 		
